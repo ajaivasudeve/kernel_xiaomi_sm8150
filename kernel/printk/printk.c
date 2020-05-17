062c3d4ba0f5e8c9cc06ee46ff42ed58a40cb6af
ajaivasudeve <ajaivasudeve@gmail.com>
2020-05-17 23:16:28 +0530

    printk: silence untracked pid logs
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -815,6 +815,7 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			line = endp;
 			/* QG-D */
 			if (strstr(line, "healthd")||
+				strstr(line, "Untracked pid")||
 				strstr(line, "cacert") ||
 				!strcmp(line, "CP: Couldn't"))
 				goto free;
